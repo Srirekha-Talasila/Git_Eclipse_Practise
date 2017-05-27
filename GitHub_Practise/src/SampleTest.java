d9b22d5c0a7d811a02e3a61cc87b73b360133097
SRIREKHA <srirekha.t@gmail.com>
2017-05-27 21:37:50 +0530

    intial check in

---
+++ GitHub_Practise/src/SampleTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+  @Test
+  public void f() {
+	  
+	  System.out.println("First TestNG Class");
+	  
+  }
+  
+  
+}
---
+++ GitHub_Practise/src/SampleTest1.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class SampleTest1 {
+	
+  @Test
+  public void f() {
+	  
+	  System.out.println("First TestNG Class");
+	  
+  }
+  
+  
+}
